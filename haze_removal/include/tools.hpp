a77231b5bf4caa1ffa22730af11ae66d14005149
GermanIris <german_iris@outlook.com>
2017-04-10 16:08:24 +0800

    add headfile

--- haze_removal/include/tools.hpp
+++ haze_removal/include/tools.hpp
@@ -2,40 +2,15 @@
 #define __TOOLS_HPP__
 
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc>
+#include <opencv2/imgproc.hpp>
 #include <limits>
 
 /** @brief Calculate Dark Channel*/
-void calcDarkChannel(const cv::Mat_<cv::Vec3b>& src, cv::Mat_<uchar>& dst, const int s)
-{
-    dst.create(src.size());
-
-    for(int i = 0; i != src.rows; ++i)
-    {
-	for(int j = 0; j != src.cols; ++j)
-	{
-	    uchar min_value = 255;
-	    int bi = i - (s >> 1), ei = bi + s,
-		bj = j - (s >> 1), ej = bj + s;
-	    for(int p = bi; p != ei; ++p)
-	    {
-		if(p < 0||p >= src.rows) continue;
-		for(int q = bj; q != ej; ++q)
-		{
-		    if(q < 0||q >= src.cols) continue;
-		    min_value = src(p, q)[0] > src(p, q)[1] ? src(p, q)[1] :
-			src(p, q)[0];
-		    if(min_value > src(p, q)[2]) min_value = src(p, q)[2];
-		}
-	    }
-	    dst(i, j) = min_value;
-	}
-    }
-}
+void calcDarkChannel(const cv::Mat_<cv::Vec3b>& src, cv::Mat_<uchar>& dst, const int s = 15);
 
 /** @brief estimate Atmospheric Light */
 
-void estimateAtmosphericLight()
+void estimateAtmosphericLight();
 
 
 #endif
